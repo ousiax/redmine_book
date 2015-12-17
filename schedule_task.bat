767caff624200a045398eb29f54f47fdb4a01ebb
qqbuby <qqbuby@gmail.com>
2015-12-17 12:15:59 +0800

    update schedule_task.bat to probe full path of python exetuable file

--- schedule_task.bat
+++ schedule_task.bat
@@ -1,16 +1,14 @@
 @rem Create a daily task on windows task scheduler.
 
-@rem Add path to Python Binaries
-@set "PythonPath="
-@if exist "C:\Python27\" set "PythonPath=C:\Python27\"
-@if exist "%ProgramFiles%\Python27\" set "PythonPath=%ProgramFiles%\Python27\"
-@if exist "%ProgramFiles(x86)%\Python27\" set "PythonPath=%ProgramFiles(x86)%\Python27\"
-@if not "%PythonPath%" == "" set PATH=%PythonPath%;%PATH%
+@rem Set pythonexe variable of python executable file full path.
+@python -c "import sys; print sys.executable" > pythonexe.tmp
+@set /p pythonexe= < pythonexe.tmp
+@del pythonexe.tmp
 
 @rem Install Pip
-@python "get-pip.py"
+@rem @python "get-pip.py"
 
 @rem Install Python-Redmine
-@pip install python-redmine --index-url="https://pypi.mirrors.ustc.edu.cn/simple"
+@rem @pip install python-redmine --index-url="https://pypi.mirrors.ustc.edu.cn/simple"
 
-@schtasks /create /tr "'%PythonPath%Python.exe' \"%CD%\book.py\"" /sc daily /tn "Redmine Book"  /st 15:00 /ru system /rl highest /f
+@echo @schtasks /create /tr "'%pythonexe%' \"%CD%\book.py\"" /sc daily /tn "Redmine Book"  /st 15:00 /ru system /rl highest /f
