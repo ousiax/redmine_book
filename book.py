b172241ab0ba8d0766d04b5635f955dd78efea2b
qqbuby <qqbuby@gmail.com>
2016-02-05 13:55:55 +0800

    Modify book.py to support random choice of comment list.

--- book.py
+++ book.py
@@ -1,22 +1,30 @@
 # !/usr/bin/env python
 # -*- encoding:utf-8 -*-
 
+import random
 from redmine_book import book
 
 
 def main():
     '''The entry point to create a time entry.'''
 
-    url = 'http://redmine.citicsinfo.com/redmine/'
-    key = '2b548161789460c36b961f62edc2c391ddc15c7c'
+    url = 'http://redmine.citicsinfo.com/redmine/'  # API URL
+    key = '1a1a1a1a1a1a1a1aa1a1a1a1a1a1'  # API访问键
 
     time_entry = {
         'issue_id': 31080,
         'hours': 8,
-        'activity_id': 9,  
-        'comments': '#31018: 资管MoM管理系统开发',
-        }
-    book(url, key, **time_entry)
+        'activity_id': 9,
+        'comments': random.choice([
+            'foo',
+            'bar',
+            'buzz',
+        ]),
+    }
+    try:
+        book(url, key, **time_entry)
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
     main()
